5bd12f1b8d6f469fc9928ef946379ae02bc979a6
Esteban Beerens <esteban.beerens@gmail.com>
2020-11-25 14:32:31 +0100

    Initial commit

---
+++ src/main/java/com/example/rederijen/RederijenApplication.java
@@ -0,0 +1,13 @@
+package com.example.rederijen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RederijenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RederijenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rederijen/RederijenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rederijen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RederijenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
