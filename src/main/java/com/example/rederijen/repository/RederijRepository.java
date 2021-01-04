c6767b307da921aa248cb5f8ed0c8b1657d53d82
kevin <kevin@KEVIN-MBP>
2021-01-04 17:11:21 +0100

    Made mongorepo string

--- src/main/java/com/example/rederijen/repository/RederijRepository.java
+++ src/main/java/com/example/rederijen/repository/RederijRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @Repository
-public interface RederijRepository extends MongoRepository<Rederij, Integer> {
+public interface RederijRepository extends MongoRepository<Rederij, String> {
 
 
     Rederij findRederijById(String id);
